311d559d8331a4120085647bf3f9feb202149093
hernok <hernok@hotmail.com>
2024-02-23 12:41:02 +0100

    added dark/lightmode logo swaps

--- src/app/sponsors/page.tsx
+++ src/app/sponsors/page.tsx
@@ -2,40 +2,72 @@ import React from 'react'
 import Image from 'next/image'
 import { client } from '../../../lib/contentful/client'
 
-const SponsorsPage = async () => {
-    /*
-	
-	let sponsors = []
+interface Sponsor {
+    fields: {
+        logoDarkmode: {
+            fields: {
+                file: {
+                    url: string
+                }
+                title: string
+            }
+        }
+        logoLightmode: {
+            fields: {
+                file: {
+                    url: string
+                }
+                title: string
+            }
+        }
+        sponsor: string
+    }
+}
 
-	//Fetching sponsors
-	try {
-		const response = await client.getEntries({ content_type: 'sponsorPage' })
-		sponsors = response.items
-	} catch (error) {
-		console.error('Error fetching logos:', error)
-		throw error
-	}
-	
-	return (
-		<div>
-		{sponsors[0].fields.sponsor.map((sponsor, index) => {
-			return (
-				<div key={index}>
-				{sponsor.fields.photo.fields.file.url && (
-					<Image
-					alt={`${sponsor.fields.name}'s picture`}
-					src={`https:${sponsor.fields.photo.fields.file.url}`}
-					width={150}
-					height={100}
-					/>
-					)}
-					<h3>{sponsor.fields.name}</h3>
-					</div>
-					)
-				})}
-				</div>
-				)
-		*/
+const SponsorsPage = async () => {
+    let sponsors = []
+
+    //Fetching sponsors
+    try {
+        const response = await client.getEntries({
+            content_type: 'sponsorsPage'
+        })
+        sponsors = response.items
+    } catch (error) {
+        console.error('Error fetching logos:', error)
+        throw error
+    }
+
+    return (
+        <div>
+            {sponsors[0].fields.sponsors.map(
+                (sponsor: Sponsor, index: number) => {
+                    return (
+                        <div key={index}>
+                            <Image
+                                src={`https:${sponsor.fields.logoDarkmode.fields.file.url}`}
+                                alt={sponsor.fields.logoDarkmode.fields.title}
+                                width={150}
+                                height={100}
+                                priority={true}
+                                className="dark:block hidden"
+                            />
+                            <Image
+                                src={`https:${sponsor.fields.logoLightmode.fields.file.url}`}
+                                alt={sponsor.fields.logoLightmode.fields.title}
+                                width={150}
+                                height={100}
+                                priority={true}
+                                className="block dark:hidden"
+                            />
+
+                            <h3>{sponsor.fields.sponsor}</h3>
+                        </div>
+                    )
+                }
+            )}
+        </div>
+    )
 }
 
 export default SponsorsPage

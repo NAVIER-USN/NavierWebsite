1856b3c3d64ef47c7db28c3f462425274c8ece6b
hernok <hernok@hotmail.com>
2024-03-17 17:30:43 +0100

    fixed map styling

--- src/components/page-home/event-map/EventMap.tsx
+++ src/components/page-home/event-map/EventMap.tsx
@@ -72,73 +72,85 @@ const EventMap = ({ eventData }: Props) => {
     })
 
     return (
-        <div className="flex flex-col justify-center w-full md:flex-row p-4 bg-foreground-light dark:bg-foreground-dark shadow-2xl">
-            <div className="w-full md:w-70vw">
-                <MapContainer
-                    center={[markers[0].latitude, markers[0].longitude]}
-                    zoom={ZOOM_LEVEL}
-                    className="h-[60vh] w-full max-w-[2000px]"
-                    scrollWheelZoom={false}
-                >
-                    <TileLayer
-                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                        attribution="&copy; contributors of OpenStreetMap"
-                    />
-
-                    {markers.map((marker, index) => (
-                        <Marker
-                            key={index}
-                            position={[marker.latitude, marker.longitude]}
+        <div className="flex flex-col items-center justify-center w-full p-4 bg-foreground-light dark:bg-foreground-dark shadow-2xl">
+            <div className="w-full max-w-[2000px]">
+                <div className="flex flex-col lg:flex-row">
+                    <div className="w-full lg:w-4/5">
+                        <MapContainer
+                            center={[markers[0].latitude, markers[0].longitude]}
+                            zoom={ZOOM_LEVEL}
+                            className="h-[60vh] w-full max-w-[2000px]"
+                            scrollWheelZoom={false}
                         >
-                            <Popup>
-                                {eventData[index].fields.eventName}
-                                <br />
-                                {eventData[index].fields.eventDescription}
-                            </Popup>
-                        </Marker>
-                    ))}
-                    <MapEffect
-                        onMapReady={(mapInstance) => {
-                            mapRef.current = mapInstance
-                        }}
-                    />
-                </MapContainer>
-            </div>
-            <div className="h-full md:pl-4 w-full md:max-w-80 overflow-y-scroll">
-                <ul className="max-h-[60vh]">
-                    {eventData.map((event, index) => (
-                        <li key={index} className="pb-2">
-                            <div
-                                className="cursor-pointer hover:underline flex flex-row justify-between select-none px-3 pb-3 border-solid border-b-2 border-text-dark dark:border-text-light"
-                                onClick={() => handleEventClick(event)}
-                            >
-                                <p className="text-lg font-semibold">
-                                    {event.fields.eventName}
-                                </p>
-                                {selectedEvent === event ? (
-                                    <SlArrowUp />
-                                ) : (
-                                    <SlArrowDown />
-                                )}
-                            </div>
-                            {selectedEvent === event && (
-                                <div className="px-2 py-1.5">
-                                    <p className="text-xl">
-                                        {event.fields.city},{' '}
-                                        {event.fields.country}
-                                    </p>
-                                    <p className="pt-3">
-                                        Date:{' '}
-                                        {formatDateTime(event.fields.eventDate)}
-                                    </p>
-                                    <p className="pt-4">
-                                        {event.fields.eventDescription}
-                                    </p>
-                                </div>
-                            )}
-                        </li>
-                    ))}
-                </ul>
+                            <TileLayer
+                                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                                attribution="&copy; contributors of OpenStreetMap"
+                            />
+
+                            {markers.map((marker, index) => (
+                                <Marker
+                                    key={index}
+                                    position={[
+                                        marker.latitude,
+                                        marker.longitude
+                                    ]}
+                                >
+                                    <Popup>
+                                        {eventData[index].fields.eventName}
+                                        <br />
+                                        {
+                                            eventData[index].fields
+                                                .eventDescription
+                                        }
+                                    </Popup>
+                                </Marker>
+                            ))}
+                            <MapEffect
+                                onMapReady={(mapInstance) => {
+                                    mapRef.current = mapInstance
+                                }}
+                            />
+                        </MapContainer>
+                    </div>
+                    <div className="md:pl-4 w-full lg:w-1/5 pt-3 lg:pt-0">
+                        <ul className="max-h-[60vh] overflow-y-auto">
+                            {eventData.map((event, index) => (
+                                <li key={index} className="pb-2">
+                                    <div
+                                        className="cursor-pointer hover:underline flex flex-row justify-between select-none px-3 pb-3 border-solid border-b-2 border-text-dark dark:border-text-light"
+                                        onClick={() => handleEventClick(event)}
+                                    >
+                                        <p className="text-lg font-semibold">
+                                            {event.fields.eventName}
+                                        </p>
+                                        {selectedEvent === event ? (
+                                            <SlArrowUp />
+                                        ) : (
+                                            <SlArrowDown />
+                                        )}
+                                    </div>
+                                    {selectedEvent === event && (
+                                        <div className="px-2 py-1.5">
+                                            <p className="text-xl">
+                                                {event.fields.city},{' '}
+                                                {event.fields.country}
+                                            </p>
+                                            <p className="pt-3">
+                                                Date:{' '}
+                                                {formatDateTime(
+                                                    event.fields.eventDate
+                                                )}
+                                            </p>
+                                            <p className="pt-4">
+                                                {event.fields.eventDescription}
+                                            </p>
+                                        </div>
+                                    )}
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                </div>
             </div>
         </div>
     )

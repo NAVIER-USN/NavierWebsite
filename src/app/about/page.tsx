279f7bc9b8f2b86ce1b6078090497371615d854a
Herman Novak <Hernok@hotmail.com>
2024-04-17 18:07:26 +0200

    finished about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { client } from '../../../lib/contentful/client'
+import Image from 'next/image'
 
 type AboutTypes = {
     fields: {
@@ -19,60 +20,88 @@ async function getContentfulData(contentType: string) {
         throw error
     }
 }
-
 const AboutPage = async () => {
     const aboutUs = await getContentfulData('aboutUsPage')
+    console.log(aboutUs.fields)
 
     return (
         <main>
-            <div className="max-w-5xl flex flex-col justify-center items-center mx-auto">
-                {aboutUs.fields.purposes.map(
-                    (purpose: AboutTypes, index: number) => (
-                        <div key={index}>
-                            <div>
-                                {purpose.fields.title && (
-                                    <h3 className="text-lg text-text-dark dark:text-text-light">
-                                        {purpose.fields.title}
-                                    </h3>
-                                )}
-                            </div>
-                            <div>
-                                <p className="text-md text-text-dark dark:text-text-light">
-                                    {purpose.fields.text}
-                                </p>
-                            </div>
-                        </div>
-                    )
-                )}
-
-                {aboutUs.fields.goals.map((goal: AboutTypes, index: number) => (
-                    <div key={index}>
-                        <div>
-                            {goal.fields.title && (
-                                <h3 className="text-lg text-text-dark dark:text-text-light">
-                                    {goal.fields.title}
-                                </h3>
-                            )}
-                        </div>
-                        <div>
-                            <p className="text-md text-text-dark dark:text-text-light">
-                                {goal.fields.text}
-                            </p>
-                        </div>
+            <div className="flex flex-col justify-center items-center mx-auto">
+                <div className="max-h-80 overflow-hidden flex flex-col justify-center items-center sm:max-w-[40vw]">
+                    <Image
+                        src={`https:${aboutUs.fields.teamGroupPhoto.fields.file.url}`}
+                        alt={aboutUs.fields.teamGroupPhoto.fields.title}
+                        priority={true}
+                        width={4000}
+                        height={2000}
+                        className="w-full object-cover"
+                    />
+                </div>
+                <div className="flex flex-col justify-center items-center max-w-5xl">
+                    <div className="pt-6 md:pt-16">
+                        {aboutUs.fields.aboutParagraphRef.map(
+                            (purpose: AboutTypes, index: number) => (
+                                <div key={index}>
+                                    <div>
+                                        {purpose.fields.title && (
+                                            <h3 className="text-xl md:text-2xl pb-3 text-text-dark dark:text-text-light text-center">
+                                                {purpose.fields.title}
+                                            </h3>
+                                        )}
+                                    </div>
+                                    <div>
+                                        <p className="px-4 text-md text-text-dark dark:text-text-light">
+                                            {purpose.fields.text}
+                                        </p>
+                                    </div>
+                                </div>
+                            )
+                        )}
                     </div>
-                ))}
+                    <div className="pt-6 md:pt-16">
+                        {aboutUs.fields.purposes.map(
+                            (purpose: AboutTypes, index: number) => (
+                                <div key={index}>
+                                    <div>
+                                        {purpose.fields.title && (
+                                            <h3 className="text-xl md:text-2xl pb-3 text-text-dark dark:text-text-light text-center">
+                                                {purpose.fields.title}
+                                            </h3>
+                                        )}
+                                    </div>
+                                    <div>
+                                        <p className="px-4 text-md text-text-dark dark:text-text-light">
+                                            {purpose.fields.text}
+                                        </p>
+                                    </div>
+                                </div>
+                            )
+                        )}
+                    </div>
+                    <div className="py-6 md:py-16">
+                        {aboutUs.fields.goals.map(
+                            (goal: AboutTypes, index: number) => (
+                                <div key={index}>
+                                    <div>
+                                        {goal.fields.title && (
+                                            <h3 className="text-xl md:text-2xl pb-3 text-text-dark dark:text-text-light text-center">
+                                                {goal.fields.title}
+                                            </h3>
+                                        )}
+                                    </div>
+                                    <div>
+                                        <p className="px-4 text-md text-text-dark dark:text-text-light">
+                                            {goal.fields.text}
+                                        </p>
+                                    </div>
+                                </div>
+                            )
+                        )}
+                    </div>
+                </div>
             </div>
         </main>
     )
 }
 
 export default AboutPage
-/*
-
-                                <div>
-                                    <p className="text-md text-text-dark dark:text-text-light">
-                                        {purpose.fields.text}
-                                    </p>
-                                </div>
-
-*/

0ec6632f30ea92c16bd8b66ef8087474f2ca367b
Herman Novak <Hernok@hotmail.com>
2024-06-12 14:31:42 +0200

    fixed unpublishing of members

--- src/components/page-member/member-card/Members.tsx
+++ src/components/page-member/member-card/Members.tsx
@@ -6,10 +6,10 @@ const Members = ({ allMembers }: MembersProps) => {
     let members: MemberEntry[] = []
     let management: MemberEntry[] = []
 
-    allMembers.member.map((member: MemberEntry) => {
-        if (member.fields.leaderRole === true) {
+    allMembers.member.forEach((member: MemberEntry) => {
+        if (member.fields && member.fields.leaderRole) {
             management.push(member)
-        } else {
+        } else if (member.fields) {
             members.push(member)
         }
     })
@@ -24,16 +24,16 @@ const Members = ({ allMembers }: MembersProps) => {
     return (
         <div>
             <div>
-                <h2 className="text-center text-2xl pt-8">wdgement</h2>
+                <h2 className="text-center text-2xl pt-8">Management</h2>
                 <div className="max-w-5xl lg:max-w-6xl grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 p-10">
                     {management.map((member: MemberEntry, index: number) => {
-                        return member ? (
+                        return member && member.fields ? (
                             <div
                                 key={index}
                                 className="bg-foreground-light dark:bg-foreground-dark rounded-br-xl overflow-hidden shadow-md"
                             >
                                 <div className="relative h-64 max-w-[256px]">
-                                    {member.fields.photo.fields.file.url && (
+                                    {member.fields.photo?.fields.file.url && (
                                         <Image
                                             alt={`${member.fields.name}'s picture`}
                                             src={`https:${member.fields.photo.fields.file.url}`}
@@ -62,13 +62,13 @@ const Members = ({ allMembers }: MembersProps) => {
                 <h2 className="text-center text-2xl pt-8">Team</h2>
                 <div className="max-w-5xl lg:max-w-6xl grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 p-10">
                     {members.map((member: MemberEntry, index: number) => {
-                        return (
+                        return member && member.fields ? (
                             <div
                                 key={index}
                                 className="bg-foreground-light dark:bg-foreground-dark rounded-br-xl overflow-hidden shadow-md flex flex-col"
                             >
                                 <div className="relative h-64 max-w-[256px]">
-                                    {member.fields.photo.fields.file.url && (
+                                    {member.fields.photo?.fields.file.url && (
                                         <Image
                                             alt={`${member.fields.name}'s picture`}
                                             src={`https:${member.fields.photo.fields.file.url}`}
@@ -93,7 +93,7 @@ const Members = ({ allMembers }: MembersProps) => {
                                     </div>
                                 </div>
                             </div>
-                        )
+                        ) : null
                     })}
                 </div>
             </div>

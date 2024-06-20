71b740eb78181e2a366c441d91cdb68f356e4154
hernok <hernok@hotmail.com>
2024-06-20 15:45:03 +0200

    added NEXT_PUBLIC to some variables

--- src/app/api/emailJs/route.ts
+++ src/app/api/emailJs/route.ts
@@ -14,8 +14,8 @@ export async function POST(request: Request) {
 
     emailjs
         .send(
-            process.env.EMAILJS_SERVICE_ID!,
-            process.env.EMAILJS_TEMPLATE_ID!,
+            process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID!,
+            process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID!,
             templateParams,
             {
                 publicKey: process.env.EMAILJS_PUBLIC_KEY!,
